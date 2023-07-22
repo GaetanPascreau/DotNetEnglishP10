cb8d882cc7e838389bf0be04ce8f36e1861e0712
Gaëtan Pascreau <62143619+GaetanPascreau@users.noreply.github.com>
2023-07-22 12:57:20 +0200

    Add project files.

---
+++ PatientDemographicsService/Models/MediscreenDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PatientDemographicsService.Models
+{
+    public class MediscreenDbContext : DbContext
+    {
+        public MediscreenDbContext(DbContextOptions<MediscreenDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ PatientDemographicsService/Models/Patient.cs
@@ -0,0 +1,14 @@
+﻿namespace PatientDemographicsService.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public char Sex { get; set; }
+        public string HomeAdress { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
