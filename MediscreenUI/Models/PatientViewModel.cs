769e20101adb2d46e1d862c7d3a497b1a5780898
Gaëtan Pascreau <62143619+GaetanPascreau@users.noreply.github.com>
2023-08-17 20:00:48 +0200

    adding a select for the sex's choice

--- MediscreenUI/Models/PatientViewModel.cs
+++ MediscreenUI/Models/PatientViewModel.cs
@@ -1,12 +1,23 @@
-﻿namespace MediscreenUI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MediscreenUI.Models
 {
     public class PatientViewModel
     {
         public int Id { get; set; }
+
+        [Required]
         public string FirstName { get; set; }
+
+        [Required]
         public string LastName { get; set; }
+
+        [Required]
         public DateTime DateOfBirth { get; set; }
+
+        [Required]
         public char Sex { get; set; }
+
         public string HomeAdress { get; set; }
         public string PhoneNumber { get; set; }
     }
